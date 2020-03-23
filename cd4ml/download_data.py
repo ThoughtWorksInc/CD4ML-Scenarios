bd34f4edbce033598a7999ad9520bf25edf0e721
David Johnston <dajohnst@thoughtworks.com>
2020-03-23 12:35:58 -0400

    change key

--- cd4ml/download_data.py
+++ cd4ml/download_data.py
@@ -2,14 +2,11 @@ import os
 import urllib.request
 import argparse
 import zipfile
-from cd4ml.filenames import file_names, data_raw
+from cd4ml.filenames import file_names, data_raw, data_source
 
 
 def load_data(path, key):
     gcsBucket = "continuous-intelligence"
-
-    if not os.path.exists(path):
-        os.makedirs(path)
 
     if not os.path.exists(os.path.join(path, key)):
         url = "https://storage.googleapis.com/%s/%s" % (gcsBucket, key)
@@ -17,8 +14,8 @@ def load_data(path, key):
 
 
 def main():
-    key = 'store47-2016.csv'
-    load_data(data_raw, key)
+    key = 'store47-2016.csv.zip'
+    load_data(data_source, key)
     zip_ref = zipfile.ZipFile(file_names['zipped_data'], 'r')
     zip_ref.extractall(data_raw)
 
