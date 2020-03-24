808875a83133f0e03c4a3fc22a026eb656b6112e
David Johnston <dajohnst@thoughtworks.com>
2020-03-24 11:52:38 -0400

    minor refactor

--- cd4ml/download_data.py
+++ cd4ml/download_data.py
@@ -6,8 +6,9 @@ from cd4ml.filenames import data_raw
 def run_download_data():
     key = 'store47-2016.csv'
     path = data_raw
-    gcsBucket = "continuous-intelligence"
+    gcs_bucket = "continuous-intelligence"
 
     if not os.path.exists(os.path.join(path, key)):
-        url = "https://storage.googleapis.com/%s/%s" % (gcsBucket, key)
-        urllib.request.urlretrieve(url, os.path.join(path, key))
+        url = "https://storage.googleapis.com/%s/%s" % (gcs_bucket, key)
+        filename = os.path.join(path, key)
+        urllib.request.urlretrieve(url, filename)
