a0f9ad8940cb113cb9399f2f096470f7c5de4b34
David Johnston <dajohnst@thoughtworks.com>
2020-03-24 13:21:09 -0400

    ensure model_dir exists

--- cd4ml/filenames.py
+++ cd4ml/filenames.py
@@ -8,18 +8,20 @@ data_dir_default = 'data'
 data_dir = os.getenv('CD4ML_DATA_DIR', data_dir_default)
 data_raw = "%s/raw" % data_dir
 data_source = "%s/source" % data_dir
+model_dir = "%s/models" % data_dir
 
 ensure_dir_exists(data_dir)
 ensure_dir_exists("%s/results" % data_dir)
 ensure_dir_exists("%s/splitter" % data_dir)
 ensure_dir_exists(data_raw)
 ensure_dir_exists(data_source)
+ensure_dir_exists(model_dir)
 
 file_names = {
     'metrics': '%s/results/metrics.json' % data_dir,
     'train': '%s/splitter/train.csv' % data_dir,
     'validation': '%s/splitter/validation.csv' % data_dir,
     'raw_data': '%s/raw/store47-2016.csv' % data_dir,
-    'model': '%s/models/model.pkl' % data_dir,
+    'model': '%s/model.pkl' % model_dir,
     'ml_params': 'ml_model_params.py'
 }
