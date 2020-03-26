9277df9915842440ee15c5489ad98a398b49ffc8
David Johnston <dajohnst@thoughtworks.com>
2020-03-26 17:42:52 -0400

    test for new reader code and one hot encoding

--- cd4ml/read_data.py
+++ cd4ml/read_data.py
@@ -1,28 +1,56 @@
-import numpy as np
-import pandas as pd
+from csv import DictReader
+from cd4ml.filenames import file_names
+from cd4ml.one_hot.one_hot_encoder import OneHotEncoder
 
 
-def read_raw_data(filename):
-    dtypes = {'class': np.str,
-              'date': np.str,
-              'day': np.str,
-              'dayoff': np.int64,
-              'dayofweek': np.str,
-              'days_til_end_of_data': np.int64,
-              'family': np.str,
-              'id': np.str,
-              'item_nbr': np.str,
-              'month': np.str,
-              'perishable': np.int64,
-              'transactions': np.int64,
-              'unit_sales': np.float64,
-              'year': np.str}
-
-    return pd.read_csv(filename, dtype=dtypes)
+def stream_raw_data():
+    filename = file_names['raw_data']
+    return (dict(row) for row in DictReader(open(filename, 'r')))
 
 
-def transform_data(data):
-    data['date'] = pd.to_datetime(data['date'], format="%Y-%m-%d")
-    data.loc[data.unit_sales < 0, 'unit_sales'] = 0
+def process(row_in):
+    row = {'id': row_in['id'],
+           'item_nbr': row_in['item_nbr'],
+           'unit_sales': max(0.0, float(row_in['unit_sales'])),
+           'date': row_in['date'],
+           'year': row_in['year'],
+           'month': row_in['month'],
+           'day': row_in['day'],
+           'class': row_in['class'],
+           'family': row_in['family'],
+           'perishable': int(row_in['perishable'] == '1'),
+           'transactions': int(row_in['transactions']),
+           'dayofweek': row_in['dayofweek'],
+           'days_til_end_of_data': int(row_in['days_til_end_of_data']),
+           'dayoff': int(row_in['dayoff'] == 'True')
+           }
 
-    # TODO: add features
+    return row
+
+
+def stream_data():
+    return (process(row) for row in stream_raw_data())
+
+
+def get_encoder_from_stream(stream):
+    categorical_n_levels_dict_all = {'item_nbr': 10000000000,
+                                     'year': 50,
+                                     'month': 13,
+                                     'day': 370,
+                                     'class': 600,
+                                     'family': 100,
+                                     'dayofweek': 10}
+
+    categorical_n_levels_dict = categorical_n_levels_dict_all
+
+    numeric_columns = ['unit_sales', 'perishable', 'transactions',
+                       'days_til_end_of_data', 'dayoff']
+
+    encoder = OneHotEncoder(categorical_n_levels_dict, numeric_columns)
+    encoder.load_from_data_stream(stream)
+    return encoder
+
+
+def get_encoder():
+    stream = stream_data()
+    return get_encoder_from_stream(stream)
---
+++ test/test_read_data.py
@@ -0,0 +1,102 @@
+from cd4ml.read_data import process, stream_raw_data, stream_data, get_encoder_from_stream
+from cd4ml.one_hot.one_hot_encoder import OneHotEncoder
+
+
+def test_stream_raw_data():
+    stream = stream_raw_data()
+    row = next(stream)
+    assert isinstance(row, dict)
+    assert 'transactions' in row
+    assert isinstance(row['transactions'], str)
+
+
+def test_stream_data():
+    stream = stream_data()
+    row = next(stream)
+    assert isinstance(row, dict)
+    assert 'transactions' in row
+    assert isinstance(row['transactions'], int)
+
+
+def test_process():
+    row_in = {'id': '88219279',
+              'date': '2016-08-16',
+              'item_nbr': '103520',
+              'unit_sales': '10.0',
+              'family': 'GROCERY I',
+              'class': '1028',
+              'perishable': '0',
+              'transactions': '3570',
+              'year': '2016',
+              'month': '8',
+              'day': '16',
+              'dayofweek': '1',
+              'days_til_end_of_data': '364',
+              'dayoff': 'False'}
+
+    row = process(row_in)
+    expected = {'id': '88219279',
+                'item_nbr': '103520',
+                'unit_sales': 10.0,
+                'date': '2016-08-16',
+                'year': '2016',
+                'month': '8',
+                'day': '16',
+                'class': '1028',
+                'family': 'GROCERY I',
+                'perishable': 0,
+                'transactions': 3570,
+                'dayofweek': '1',
+                'days_til_end_of_data': 364,
+                'dayoff': 0}
+
+    assert row == expected
+
+
+def test_get_encoder_from_stream():
+    stream = stream_data()
+    stream_small = (next(stream) for _ in range(100))
+    encoder = get_encoder_from_stream(stream_small)
+    assert isinstance(encoder, OneHotEncoder)
+
+    stream = stream_data()
+    stream_small = (next(stream) for _ in range(100))
+    encoded = list(encoder.encode_data_stream(stream_small))
+    assert len(encoded) == 100
+
+    row_in = {'id': '88219279',
+              'date': '2016-08-16',
+              'item_nbr': '103520',
+              'unit_sales': '10.0',
+              'family': 'GROCERY I',
+              'class': '1028',
+              'perishable': '0',
+              'transactions': '3570',
+              'year': '2016',
+              'month': '8',
+              'day': '16',
+              'dayofweek': '1',
+              'days_til_end_of_data': '364',
+              'dayoff': 'False'}
+
+    encoded = encoder.encode_data([row_in])
+    decoded = encoder.decode_data(encoded)
+
+    print(decoded[0].keys())
+    print(row_in.keys())
+    del row_in['date'], row_in['id']
+
+    assert decoded[0].keys() == row_in.keys()
+
+    #  make a level not seen
+    row_in['class'] = 'FOO'
+
+    encoded = encoder.encode_data([row_in])
+    decoded = encoder.decode_data(encoded)
+
+    print(decoded[0].keys())
+    print(row_in.keys())
+
+    assert decoded[0].keys() == row_in.keys()
+    print(decoded)
+    assert decoded[0]['class'] == 'UNKNOWN_CATEGORICAL_LEVEL'
