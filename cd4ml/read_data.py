164ab3e4a0f4e207fb91cbc483c512f3e6cc7aaa
David Johnston <dajohnst@thoughtworks.com>
2020-03-26 12:44:51 -0400

    fix flake8 complaint

--- cd4ml/read_data.py
+++ cd4ml/read_data.py
@@ -5,13 +5,3 @@ from cd4ml.filenames import file_names
 def stream_raw():
     filename = file_names['raw_data']
     return (dict(row) for row in DictReader(open(filename, 'r')))
-
-
-def add_feratures(row):
-    add_feature_1(row)
-    add_feature_2(row)
-
-
-
-
-
