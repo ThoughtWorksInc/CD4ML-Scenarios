493b9e0d85aa6be4b7eba481d63d4d621d7f6fa7
David Johnston <dajohnst@thoughtworks.com>
2020-04-09 16:43:48 -0400

    max_features 0.5

--- cd4ml/ml_model_params.py
+++ cd4ml/ml_model_params.py
@@ -1,7 +1,7 @@
 model_parameters = {
     "random_forest": {
         "n_estimators": 50,
-        "max_features": 0.3
+        "max_features": 0.5
     },
     "adaboost": {
         "n_estimators": 100
