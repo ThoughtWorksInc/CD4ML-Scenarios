abdd14b728eaa63472a571816303f72667819687
David Johnston <dajohnst@thoughtworks.com>
2020-04-09 13:13:30 -0400

    fix adaboost param name

--- cd4ml/ml_model_params.py
+++ cd4ml/ml_model_params.py
@@ -3,11 +3,6 @@ model_parameters = {
         "n_estimators": 50
     },
     "adaboost": {
-        "n_estimators": 50,
-        "learning_rate": 1.0,
-        "loss": "linear"
-    },
-    "adaboost_regressor": {
         "n_estimators": 200,
         "max_depth": 4
     },
