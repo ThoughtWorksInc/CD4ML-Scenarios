3ca3bba796bf133e8585f8766ae820a80a06472f
David Johnston <dajohnst@thoughtworks.com>
2020-04-09 00:43:31 -0400

    change acceptance threshold back

--- cd4ml/accept_model.py
+++ cd4ml/accept_model.py
@@ -22,7 +22,7 @@ def check_model_performance(metric_name, threshold_min, threshold_max):
     last_run_record = get_latest_executed_run(runs)
     metric_value = get_metric(metric_name, last_run_record)
     run_name = last_run_record["tags.mlflow.runName"][0]
-    template = "{metric_name} for Run: {run_name} was not accepted, " \
+    template = "Metric: {metric_name} for Run: {run_name} was not accepted, " \
                "value: {metric_value}, " \
                "threshold_min: {threshold_min}, threshold_max: {threshold_max}"
     message = template.format(run_name=run_name,
