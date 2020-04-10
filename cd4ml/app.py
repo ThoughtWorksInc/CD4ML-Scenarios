a4880ad689ea804643b229555bfbf2d27aed8dab
David Johnston <dajohnst@thoughtworks.com>
2020-04-10 18:40:24 -0400

    remove debugging calls to fluentd logger

--- cd4ml/app.py
+++ cd4ml/app.py
@@ -45,7 +45,6 @@ def get_prediction():
 
     log_prediction_console(log_payload)
     fluentd_logger.log('prediction', log_payload)
-    fluentd_logger.log('foo', {'bar': 'buzz'})
 
     if status == "ERROR":
         return prediction, 503
