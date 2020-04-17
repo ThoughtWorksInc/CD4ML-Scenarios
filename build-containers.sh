db0b702008d708f844c1ca06259c09e4765d5c4e
Eric Nagler <eric.nagler@thoughtworks.com>
2020-04-16 20:09:52 -0400

    Adjust containers to use odsc-east-2020 docker tag

--- build-containers.sh
+++ build-containers.sh
@@ -1,21 +1,14 @@
 # Build all of the containers
 
 echo "Building Jenkins Master"
-docker build --rm -f Dockerfile-jenkins -t ericnaglertw/cd4ml-build-master:2 .
+docker build --rm -f Dockerfile-jenkins -t ericnaglertw/cd4ml-build-master:odsc-east-2020 .
 echo "Building MLFlow"
-docker build --rm -f Dockerfile-mlflow -t ericnaglertw/cd4ml-mlflow:1 .
+docker build --rm -f Dockerfile-mlflow -t ericnaglertw/cd4ml-mlflow:odsc-east-2020 .
 echo "Building Model Server"
-docker build --rm -f Dockerfile-model -t ericnaglertw/cd4ml-model-server:1 .
+docker build --rm -f Dockerfile-model -t ericnaglertw/cd4ml-model-server:odsc-east-2020 .
 
 echo "Building Fluentd"
 pushd .
 cd fluentd
-docker build --rm  -f Dockerfile -t ericnaglertw/cd4ml-fluentd:1 .
+docker build --rm  -f Dockerfile -t ericnaglertw/cd4ml-fluentd:odsc-east-2020 .
 popd
-
-# Push to DockerHub
-
-# docker push ericnaglertw/cd4ml-build-master:2
-# docker push ericnaglertw/cd4ml-mlflow:1
-# docker push ericnaglertw/cd4ml-fluentd:1
-# docker push ericnaglertw/cd4ml-model-server:1
