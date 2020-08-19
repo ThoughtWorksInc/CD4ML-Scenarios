fa53c0ac9bb5e1a6e6bde60ef695735aa1f446ef
Eric Nagler <eric.nagler@thoughtworks.com>
2020-08-18 21:17:02 -0400

    Correct webapp data_scorer to use float_or_zero

--- cd4ml/webapp/webapp_data_scorer.py
+++ cd4ml/webapp/webapp_data_scorer.py
@@ -1,13 +1,26 @@
 import logging
 
+from cd4ml.utils.utils import float_or_zero
+
 logger = logging.getLogger(__name__)
+def form_values_to_input_data(form_data, numeric_cols):
+    # apply the right schema
+    input_data = {}
+    for key, val in form_data.items():
+        if key in numeric_cols:
+            input_data[key] = float_or_zero(val)
+        else:
+            input_data[key] = str(val)
+
+    return input_data
 
 
 def get_form_from_model(spec_name, identifier, model, initial_values=None):
     omitted_fields = model.feature_set.omitted_feature_fields_for_input()
 
     if initial_values is not None:
-        input_data = initial_values
+        numeric_cols = model.feature_set.ml_fields()['numerical']
+        input_data = form_values_to_input_data(initial_values, numeric_cols)
     else:
         input_data = None
 
