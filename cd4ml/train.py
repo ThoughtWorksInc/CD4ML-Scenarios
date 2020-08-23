57ef12da6f910e258560f3b96b32e51bb3ffdb7d
David Johnston <dajohnst@thoughtworks.com>
2020-08-23 01:48:22 -0400

    print statement on rows and cols of train data

--- cd4ml/train.py
+++ cd4ml/train.py
@@ -7,5 +7,8 @@ def train_model(train_data, target, model_name, params, seed=None):
     print("Training %s model" % model_name)
     clf = model_class(random_state=seed, **params)
 
+    n_rows = len(train_data)
+    n_cols = len(train_data[0])
+    print('n_rows: %s, n_cols: %s' % (n_rows, n_cols))
     trained_model = clf.fit(train_data, target)
     return trained_model, params
