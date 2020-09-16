dbf2209ab111eb5fd468fd51fa7f975c30c522fa
David Johnston <dajohnst@thoughtworks.com>
2020-09-15 20:37:19 -0400

    version bump for wickedhot to remove the min, max validators on the form

--- local_app.sh
+++ local_app.sh
@@ -2,6 +2,8 @@ export FLUENTD_HOST="fluentd"
 export FLUENTD_PORT="24224"
 export FLASK_APP="cd4ml/app.py"
 export FLASK_ENV="production"
+export MLFLOW_TRACKING_URL='http://mlflow:5000'
+export MLFLOW_S3_ENDPOINT_URL = 'http://minio:9000'
 
 pip3 install -r requirements.txt
 # EXPOSE 5005
